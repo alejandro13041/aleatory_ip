a37aedf540c70c1abe91f3940cce93229a248c9d
alejandro13041 <83832792+alejandro13041@users.noreply.github.com>
2021-09-30 12:44:26 -0500

    Update bibliotecas.h

--- bibliotecas.h
+++ bibliotecas.h
@@ -4,195 +4,189 @@
 #include <windows.h>
 #include <time.h>
 #include <conio.h>
-
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <array>
 using namespace std;
-void class_ip(long IP_H,string IPC_H[]){
- 
-  volatile unsigned long I1 = 0;
-  volatile unsigned long I2 = 0;
-  volatile unsigned long I3 = 0;
-  volatile unsigned long I4 = 0;
-  volatile unsigned long I5 = 0;
 
 
-  for (int i = 0; i < IP_H; ++i){
+void clasification_counter_ip(long iph,string IPch[]){
 
-    if (IPC_H[i] == "A"){
+ unsigned long i1 = 0;
+ unsigned long i2 = 0;
+ unsigned long i3 = 0;
+ unsigned long i4 = 0;
+ unsigned long i5 = 0;
 
-      I1++;
+	 for (int i = 0; i < iph; ++i){
 
-     }else if (IPC_H[i] == "B"){
+		  if(IPch[i] =="A"){
 
-      I2++;
+			 i1++;
 
-     }else if (IPC_H[i] == "C"){
+			 }else if (IPch[i] =="B"){
 
-      I3++;
+			 i2++;
 
-     }
-    else if (IPC_H[i] == "D"){
+			 }else if (IPch[i] =="C"){
 
-      I4++;
+			 i3++;
 
-     }
-    else if (IPC_H[i] == "E"){
+			 }else if (IPch[i] =="D"){
 
-      I5++;
+			 i4++;
 
-     }else{
+			 }else if (IPch[i] =="E"){
 
-      continue;
+			 i5++;
 
-     }
+			 } 
+			 else{
 
-   }
+			 continue;
 
-   cout<<"[IPs:A]"<<I1<<"[IPs:B]"<<I2<<"[IPs:C]"<<I3<<"[IPs:D]"<<I4<<"[IPs:E]"<<I5;
+			 }
+		
+	 }
+
+	 cout<<"complete";
+	 cout<<".";
+	 Sleep(1000);
+	 cout<<".";
+	 Sleep(1000);
+	 cout<<"."<<endl;  
+	 cout<<endl;
+
+	 cout<<"[A]:"<<i1<<endl;
+	 cout<<"[B]:"<<i2<<endl;
+	 cout<<"[C]:"<<i3<<endl;
+	 cout<<"[D]:"<<i4<<endl;
+	 cout<<"[E]:"<<i5<<endl;
 
  }
 void aleatory_ip(long ip,string IPs[],string IPc[]){
 
-     volatile int n1[ip];
-     volatile int n2[ip];
-     volatile int n3[ip];
-     volatile int n4[ip];
+ volatile int n1[ip];
+ volatile int n2[ip];
+ volatile int n3[ip];
+ volatile int n4[ip];
 
-     int j  = 0;
-     int k  = 0;
+ int j  = 0;
+ int k  = 0;
 
-     string IPa[ip]; 
+ string IPa[ip]; 
 
-      for (int i = 0; i < ip; ++i){  
+	for (int i = 0; i < ip; ++i){  
 
-        n1[i] = 0 + rand() % 255;
-        n2[i] = 0 + rand() % 255;
-        n3[i] = 0 + rand() % 255;
-        n4[i] = 0 + rand() % 255;
+		n1[i] = 0 + rand() % 255;
+		n2[i] = 0 + rand() % 255;
+		n3[i] = 0 + rand() % 255;
+		n4[i] = 0 + rand() % 255;
 
-      }    
-      for (int i = 0; i < ip; ++i){       
-       
-       IPs[i] = to_string(n1[i]) + '.' +
-                to_string(n2[i]) + '.' + 
-                to_string(n3[i]) + '.' + 
-                to_string(n4[i]) ;
+	 }    
+	for (int i = 0; i < ip; ++i){       
+	 
+	 IPs[i] = to_string(n1[i]) + '.' +
+						to_string(n2[i]) + '.' + 
+						to_string(n3[i]) + '.' + 
+						to_string(n4[i]) ;
 
-       }
+	 }
+	for (int i = 0; i <ip; ++i){
 
+	 (n1[i]>=0   and n1[i]<=127) ? IPc[i]="A":IPc[i]=IPc[i];
+	 (n1[i]>=128 and n1[i]<=191) ? IPc[i]="B":IPc[i]=IPc[i];
+	 (n1[i]>=192 and n1[i]<=233) ? IPc[i]="C":IPc[i]=IPc[i];
+	 (n1[i]>=224 and n1[i]<=239) ? IPc[i]="D":IPc[i]=IPc[i];
+	 (n1[i]>=240 and n1[i]<=255) ? IPc[i]="E":IPc[i]=IPc[i];
+						
+	 }         
+	for (int i = 0; i < ip; ++i){
 
-      for (int i = 0; i <ip; ++i){
-    
-       (n1[i]>=0   and n1[i]<=127) ? IPc[i]="A":IPc[i]=IPc[i];
-       (n1[i]>=128 and n1[i]<=191) ? IPc[i]="B":IPc[i]=IPc[i];
-       (n1[i]>=192 and n1[i]<=233) ? IPc[i]="C":IPc[i]=IPc[i];
-       (n1[i]>=224 and n1[i]<=239) ? IPc[i]="D":IPc[i]=IPc[i];
-       (n1[i]>=240 and n1[i]<=255) ? IPc[i]="E":IPc[i]=IPc[i];
-                
-       }         
-      for (int i = 0; i < ip; ++i){
+			if (IPs[i] =="127.0.0.0")
+		 {
+			 continue;
+		 }
+		 else if (IPs[i] =="0.0.0.0")
+		 {
+			 continue;
+		 }
+		 else if (IPs[i] =="3.0.0.0")
+		 {
+			 continue;
+		 }
+		 else if (IPs[i] =="15.0.0.0")
+		 {
+			 continue;
+		 }
+		 else if (IPs[i] =="56.0.0.0")
+		 {
+			 continue;
+		 }
+		 else if (IPs[i] =="10.0.0.0")
+		 {
+			 continue;
+		 }
+		 else if (IPs[i] =="192.168.0.0")
+		 {
+			 continue;
+		 }
+		 else if (IPs[i] =="172.16.0.0")
+		 {
+			 continue;
+		 }
+		 else if (IPs[i] =="100.64.0.0")
+		 {
+			 continue;
+		 }
+		 else{ 
 
-                  if (IPs[i] =="127.0.0.0")
-                 {
-                   continue;
-                 }
-                 else if (IPs[i] =="0.0.0.0")
-                 {
-                   continue;
-                 }
-                 else if (IPs[i] =="3.0.0.0")
-                 {
-                   continue;
-                 }
-                 else if (IPs[i] =="15.0.0.0")
-                 {
-                   continue;
-                 }
-                 else if (IPs[i] =="56.0.0.0")
-                 {
-                   continue;
-                 }
-                 else if (IPs[i] =="10.0.0.0")
-                 {
-                   continue;
-                 }
-                 else if (IPs[i] =="192.168.0.0")
-                 {
-                   continue;
-                 }
-                 else if (IPs[i] =="172.16.0.0")
-                 {
-                   continue;
-                 }
-                 else if (IPs[i] =="100.64.0.0")
-                 {
-                   continue;
-                 }
-                 else{ 
-                   IPa[j] = IPs[i];
-                   j++;
-                 }
-               }
+			 IPa[j] = IPs[i];
+			 j++;
 
+		 }
+	 }
+	for (int i = 0; i < ip; ++i){
 
-                 for (int i = 0; i < ip; ++i){
+		IPs[i] = IPa[i];
 
-                    IPs[i] = IPa[i];
+	 }
 
-                   }
+	j = 0;
+	k = 0;
 
-                    j = 0;
-                    k = 0;
+	for(int i = 0; i < ip; i++){
 
-            for(int i = 0; i < ip; i++){
-    
-              for(j = i + 1; j < ip; j++){
-    
-                if(IPs[i] == IPs[j]){
-     
-                     k = j;
-     
-                     while(k < ip -1){
-    
-                        IPs[k] = IPs[k+1]; 
-     
-                        ++k;
-                    }
-     
-                    --ip;
-                    --j;
-     
-                
-                  }
-     
-                }
-     
-             } 
+		for(j = i + 1; j < ip; j++){
 
-      cout<<"complete";
-      cout<<".";
-      Sleep(1000);
-      cout<<".";
-      Sleep(1000);
-      cout<<".";             
- }
-void test_case(){
+			if(IPs[i] == IPs[j]){
 
- system("color 6");
+					 k = j;
 
- long IP; 
- string IPS[IP];
- string IPC[IP];
+					 while(k < ip -1){
 
-system("time /T");
-  cin>>IP;
-  aleatory_ip(IP,IPS,IPC);
+							IPs[k] = IPs[k+1]; 
 
-  for (int i = 0; i < IP; ++i){
+							++k;
+					}
 
-    cout<<"["<<i+1<<"]"<<"["<<IPC[i]<<"]"<<"["<<IPS[i]<<"]"<<endl;
+					--ip;
+					--j;
 
-   }
+			
+				}
 
-  class_ip(IP,IPC);
-  
+			}
+
+	 }
+ 
+	cout<<"complete";
+	cout<<".";
+	Sleep(1000);
+	cout<<".";
+	Sleep(1000);
+	cout<<"."<<endl; 
+	cout<<endl; 
+			
  }
