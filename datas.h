169c174093309a19bf58ac6651e24f5ac78a712f
alejandro13041 <83832792+alejandro13041@users.noreply.github.com>
2021-10-09 14:19:55 -0500

    Add files via upload

---
+++ datas.h
@@ -0,0 +1,152 @@
+#include <iostream>
+
+using namespace std;
+
+ struct ussers{
+
+  char usser1 [10] = "root";
+  char usser2 [10] = "root";
+  char usser3 [10] = "root";
+  char usser4 [10] = "admin";
+  char usser5 [10] = "root";
+  char usser6 [10] = "root";
+  char usser7 [10] = "root";
+  char usser8 [10] = "root";
+  char usser9 [10] = "root";
+  char usser10[10] = "root";
+  char usser11[10] = "support";
+  char usser12[10] = "root";
+  char usser13[10] = "admin";
+  char usser14[10] = "root";
+  char usser15[10] = "root";
+  char usser16[10] = "user";
+  char usser17[10] = "admin";
+  char usser18[10] = "root";
+  char usser19[10] = "admin";
+  char usser20[10] = "root";
+  char usser21[10] = "admin";
+  char usser22[10] = "admin";
+  char usser23[10] = "root";
+  char usser24[10] = "root";
+  char usser25[10] = "root";
+  char usser26[10] = "root";
+  char usser27[10] = "service";
+  char usser28[10] = "guest";
+  char usser29[10] = "guest";
+  char usser30[10] = "guest";
+  char usser31[10] = "admin1";
+  char usser32[10] = "666666";
+  char usser33[10] = "888888";
+  char usser34[10] = "ubnt ";
+  char usser35[10] = "root";
+  char usser36[10] = "root";
+  char usser37[10] = "root";
+  char usser38[10] = "root";
+  char usser39[10] = "root";
+  char usser40[10] = "root"; 
+  char usser41[10] = "root";
+  char usser42[10] = "root";
+  char usser43[10] = "root";
+  char usser44[10] = "root";
+  char usser45[10] = "root";
+  char usser46[10] = "root";
+  char usser47[10] = "root";
+  char usser48[10] = "root";
+  char usser49[10] = "admin";
+  char usser50[10] = "admin";
+  char usser51[10] = "admin";
+  char usser52[10] = "admin";
+  char usser53[10] = "admin";
+  char usser54[10] = "admin";
+  char usser55[10] = "admin";
+  char usser56[10] = "admin";
+  char usser57[10] = "admin";
+  char usser58[10] = "admin";
+  char usser59[10] = "admin";
+  char usser60[10] = "admin";
+  char usser61[10] = "tech";
+  char usser62[10] = "mother";
+
+ }usser, *punter_usser = &usser;
+
+ struct passwords{
+
+   char password1[10] ="xc3511";
+   char password2[10] ="vizxv";
+   char password3[10] ="admin";
+   char password4[10] ="admin";
+   char password5[10] ="888888";
+   char password6[10] ="xmhdipc";
+   char password7[10] ="default";
+   char password8[10] ="juantech";
+   char password9[10] ="123456";
+   char password10[10] ="54321";
+   char password11[10] ="support";
+   char password12[10] ="";
+   char password13[10] ="password";
+   char password14[10] ="root";
+   char password15[10] ="12345";
+   char password16[10] ="user";
+   char password17[10] ="";
+   char password18[10] ="pass";
+   char password19[10] ="admin1234";
+   char password20[10] ="1111";
+   char password21[10] ="smcadmin";
+   char password22[10] ="1111";
+   char password23[10] ="666666";
+   char password24[10] ="password";
+   char password25[10] ="1234";
+   char password26[10] ="klv123";
+   char password27[10] ="admin";
+   char password28[10] ="service";
+   char password29[15] ="supervisor";
+   char password30[10] ="guest";
+   char password31[10] ="12345";
+   char password32[10] ="12345";
+   char password33[10] ="password";
+   char password34[10] ="1234";
+   char password35[10] ="666666";
+   char password36[10] ="888888";
+   char password37[10] ="ubnt";
+   char password38[10] ="klv1234";
+   char password39[10] ="Zte521";
+   char password40[10] ="hi3518";
+   char password41[10] ="jvbzd";   
+   char password42[10] ="anko";
+   char password43[10] ="zlxx.";
+   char password44[15] ="7ujMko0vizxv";
+   char password45[15] ="7ujMko0admin";
+   char password46[10] ="system";
+   char password47[10] ="ikwb";
+   char password48[10] ="dreambox";
+   char password49[10] ="user";
+   char password50[10] ="realtek";
+   char password51[10] ="00000000";
+   char password52[10] ="1111111";
+   char password53[10] ="1234";
+   char password54[10] ="12345";
+   char password55[10] ="54321";
+   char password56[10] ="123456";
+   char password57[20] ="7ujMko0admin";
+   char password58[10] ="1234";
+   char password59[10] ="pass";
+   char password60[10] ="meinsm";
+   char password61[1010] ="tech";
+   char password62[10] ="fucker";
+
+   }password ,*punter_password = &password;
+   
+   
+
+int main(){
+  
+  cout<<"[scanner:]"<<punter_password->password1;
+  cout<<"[scanner:]"<<punter_password->password2;
+  cout<<"[scanner:]"<<punter_password->password3;
+  cout<<"[scanner:]"<<punter_usser->usser1;
+  cout<<"[scanner:]"<<punter_usser->usser2;
+  cout<<"[scanner:]"<<punter_usser->usser3;
+  
+
+  return 0;
+ }
