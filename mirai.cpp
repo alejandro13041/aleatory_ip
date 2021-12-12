45624d8ecc0f4c8374a7fd44f8330ec2234367d8
alejandro13041 <83832792+alejandro13041@users.noreply.github.com>
2021-12-12 10:24:26 -0500

    Update mirai.cpp

--- mirai.cpp
+++ mirai.cpp
@@ -1,24 +1,28 @@
 #include "bibliotecas.h"
 using namespace std;
-void test_case(){
+void test_case(void){
 
+
+
+   cout<<"[scanner] Create _13041"<<endl;
    system("color 6");
    long IP;
    cin>>IP;
    string IPS[IP];
    string IPC[IP];
+   long PTS[IP];
    aleatory_ip(IP,IPS,IPC);
-
+   aleatory_port(IP,PTS);
    for (int i = 0; i < IP; ++i){
 
-      cout<<"["<<i+1<<"]"<<"["<<IPC[i]<<"]:"<<IPS[i]<<endl;
+      cout<<"["<<i+1<<"]"<<"["<<IPC[i]<<"]:"<<IPS[i]<<":"<<PTS[i]<<endl;
       Sleep(500);
 
     }
-
+  
     clasification_counter_ip(IP,IPC);
-    IPS.fill("");
-    IPC.fill("");
+    Sleep(16000);
+    
  }
 int main(){
 
@@ -31,4 +35,5 @@ int main(){
 
  return 0;
  }
+
 
