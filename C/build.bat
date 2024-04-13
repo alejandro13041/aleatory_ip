76f2ab9f38fb2e0d7ad6b6a4249c96e9add05b9f
alejandro13041 <83832792+alejandro13041@users.noreply.github.com>
2024-04-12 22:57:29 -0500

    Update build.bat
    add code for compile

--- C/build.bat
+++ C/build.bat
@@ -1,1 +1,13 @@
+@echo off
+	
+echo start compiling proyect.
+cd src
 
+gcc -Wall -Wextra -c IP.c
+gcc -Wall -Wextra -c count.c 
+gcc -Wall -Wextra -c country.c
+gcc -Wall -Wextra -c database.c 
+gcc -Wall -Wextra -c information.c
+
+gcc -c main.c IP.c count.c country.c database.c information.c -o main.exe 
+pause
